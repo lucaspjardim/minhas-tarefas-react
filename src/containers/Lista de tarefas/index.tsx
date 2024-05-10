cc18a645bc601a29f70ee483d053b4f9686c2a96
Lucas Jardim <lucas.pasqualijardim59@gmail.com.br>
2024-05-10 16:23:04 -0300

    Ajustando os botões

--- src/containers/Lista de tarefas/index.tsx
+++ src/containers/Lista de tarefas/index.tsx
@@ -1,16 +1,34 @@
 import Tarefa from "../../components/Tarefa"
 import { Container } from "./style"
 
+const tarefas = [
+  {
+    titulo: 'Estudar',
+    descricao: 'Ver a aula 3',
+    prioridade: 'urgente',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Estudar',
+    descricao: 'Ver a aula 8',
+    prioridade: 'normal',
+    status: 'concluida'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-      </li>
+      {tarefas.map(t => (
+        <li key={t.titulo}>
+        <Tarefa
+          descricao={t.descricao}
+          titulo={t.titulo}
+          status={t.status}
+          prioridade={t.prioridade} />
+        </li>
+      ))}
     </ul>
   </Container>
 )
